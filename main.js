7a732ce3fe3e0aa6e42c54049a8b0f2ff3c51b1c
Prakash8782 <prakash8782@gmail.com>
2022-09-12 12:26:13 +0530

    function

---
+++ main.js
@@ -0,0 +1,24 @@
+// Function Expression
+
+const prakash = function (x,y){
+    return z=x*y;
+        
+}
+console.log(prakash(7,5));
+console.log(prakash);
+
+// Function Declaration
+        
+function praveen(a,b){
+    return c=a+b;
+}
+console.log(praveen);
+console.log(praveen(3,4));
+
+// Arrow Function
+
+const bike=()=>{
+    let bname= 'Bullet';
+    return bname;
+}
+console.log(bike());
